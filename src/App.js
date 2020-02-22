af9e1899c532d23d5210442aed998122b71f3d19
mano <mano@neerap.com>
2020-02-22 23:01:18 +0530

    Initial react code

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          <code>stay tuned, app design in progress</code> 
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
