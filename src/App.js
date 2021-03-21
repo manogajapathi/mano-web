d6736c205eede42df9495bee40a9fa28f5f4eb2b
Mano <manogajapathi@gmail.com>
2021-03-22 01:12:29 +0530

    routing

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
+import {Route} from 'react-router-dom';
+import { withRouter } from 'react-router'
 import MainPage from './components/MainPage';
 import './App.css';
 
 function App() {
+  const main = () => <MainPage />
   return (
     <div>
-      <MainPage />
+      <Route exact path="/" component={main} />
     </div>
   );
 }
 
-export default App;
+export default withRouter(App);
