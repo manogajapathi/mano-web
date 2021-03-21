46deb8dffe36249c6be6ce48fd52e5b874b1245a
Mano <manogajapathi@gmail.com>
2021-03-22 01:19:12 +0530

    Updates

---
+++ precache-manifest.5d59ac320f77465d7c9ce23f9a58545c.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cad7289b6b8db56aa4c240f0340d906a",
+    "url": "./index.html"
+  },
+  {
+    "revision": "9dbea39c13f1f71dbc8a",
+    "url": "./static/css/main.4a04a549.chunk.css"
+  },
+  {
+    "revision": "25c8b674bf218c9c6072",
+    "url": "./static/js/2.b5a6d615.chunk.js"
+  },
+  {
+    "revision": "9fd3a780d50ebc7200a9f6d069323925",
+    "url": "./static/js/2.b5a6d615.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9dbea39c13f1f71dbc8a",
+    "url": "./static/js/main.b267b14f.chunk.js"
+  },
+  {
+    "revision": "e7bc8bc33111db69900c",
+    "url": "./static/js/runtime-main.94286e23.js"
+  },
+  {
+    "revision": "44be1ccd70f33558088c75756c331dd3",
+    "url": "./static/media/fb.44be1ccd.png"
+  },
+  {
+    "revision": "f94a066e0d5ffe7b02c3864e4676b3b1",
+    "url": "./static/media/info.f94a066e.png"
+  },
+  {
+    "revision": "097b77494452f9352630bcf79697db4d",
+    "url": "./static/media/insta.097b7749.png"
+  },
+  {
+    "revision": "82e0ccb38c44cee91a38ec2e3c453344",
+    "url": "./static/media/mano.82e0ccb3.jpg"
+  },
+  {
+    "revision": "457c5fe8bc7bb1fa6ca569c85f0c99fa",
+    "url": "./static/media/mano2.457c5fe8.jpg"
+  },
+  {
+    "revision": "793a3f5d20b8c3f91c951263634eff32",
+    "url": "./static/media/smule.793a3f5d.png"
+  },
+  {
+    "revision": "188a278629872508123e7bf25a4e4ae9",
+    "url": "./static/media/wp.188a2786.png"
+  },
+  {
+    "revision": "bf3e25852e72e2d84fc445f423d8219d",
+    "url": "./static/media/ytube.bf3e2585.png"
+  }
+]);
